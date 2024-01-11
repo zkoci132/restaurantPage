86ae9b8700cc715ff9af795253491b8bf4f69572
zkoci132 <zkoci132@csitrd.kutztown.edu>
2024-01-10 19:01:00 -0500

    fixed config

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,15 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: {
-        index: './src/index.js'
-    },
+    entry: './src/index.js',
     resolve: {
         extensions: ['.js']
     },
     devtool: 'inline-source-map',
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+        
     },
     module: {
         rules: [
